fd5ce335ffc1b8a820dff6f9dc2f2c2671d6c9ca
tsuchiyaaki <a.tsuchiya.d.f@gmail.com>
2026-07-04 14:29:23 +0900

    あ

---
+++ SNS_mokuhyou/NewPostView.swift
@@ -0,0 +1,7 @@
+//
+//  Newpost.swift
+//  SNS_mokuhyou
+//
+//  Created by 土屋　暁 on 2026/07/03.
+//
+
---
+++ SNS_mokuhyou/SearchView.swift
@@ -0,0 +1,13 @@
+//
+//  SarchView.swift
+//  SNS_mokuhyou
+//
+//  Created by 土屋　暁 on 2026/06/21.
+//
+
+import SwiftUI
+struct SarchView: View {
+    var body: some View {
+        SearchFieldPlacement
+    }
+}
